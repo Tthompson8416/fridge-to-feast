7dcd667f84119f17a8b321675258aac90b48d7a7
TT84 <tthompson8416@outlook.com>
2024-06-21 16:53:45 -0400

    Edamam API connected and intial changes made to CSS and JavaScript

--- Assets/JS/script4.js
+++ Assets/JS/script4.js
@@ -1,12 +1,69 @@
 const appId = "9a69a94c";
 const appKey = "63a596b84513443fc062e1e803e21980";
 const baseUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${appId}&app_key=${appKey}`;
+const recipeContainer = document.querySelector("#recipe-container");
+const txtSearch = document.querySelector("#txtSearch");
+const btnFind = document.querySelector(".btn");
+const loadingEle = document.querySelector("#loading");
+
+btnFind.addEventListener("click", () => loadRecipies(txtSearch.value));
+
+txtSearch.addEventListener("keyup", (e) => {
+    const inputVal = txtSearch.value;
+    if (e.keyCode === 13) {
+        loadRecipies(inputVal)
+    }
+});
+
+const toggleLoad = (element, isShow) => {
+    element.classList.toggle("hide", isShow);
+};
+const setScrollPosition = () => {
+    recipeContainer.scrollTo({top: 0, behavior: "smooth"});
+};
 
 function loadRecipies(type = "paneer") {
+    toggleLoad(loadingEle, false);
     const url = baseUrl + `&q=${type}`;
     fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data.hits))
-    .catch((error) => console.log(error));
+    .then((data) => {
+        renderRecipes(data.hits);
+        toggleLoad(loadingEle, true);
+    })
+    .catch((error) => toggleLoad(loadingEle, true))
+    .finally(() => setScrollPosition());
 }
 loadRecipies();
+
+const getRecipeStepsStr = (ingredientLines = []) => {
+    let str = "";
+    for (var step of ingredientLines) {
+        str = str+`<li>${step}</li>`
+    }
+    return str;
+};
+
+const renderRecipes = (recipeList = []) => {
+    recipeContainer.innerHTML = "";
+recipeList.forEach((recipeObj) => {
+    const {
+        label: recipeTitle,
+        ingredientLines,
+        image: recipeImage,
+    } = recipeObj.recipe;
+    const recipeStepsStr=getRecipeStepsStr(ingredientLines);
+    const htmlStr = `<div class="recipe">
+            <div class="recipe-title">${recipeTitle}</div>
+            <div class="recipe-img">
+                <img src="${recipeImage}" />
+            </div>
+            <div class="recipe-text">
+                <ul>
+                    ${recipeStepsStr}
+                </ul>
+            </div>
+         </div>`;
+         recipeContainer.insertAdjacentHTML("beforeend", htmlStr)
+});
+};
